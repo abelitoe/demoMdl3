e4f56a57b84d8763473e5bc5180cbd85896c0ae5
abelitoe <abelmayuri01@gmail.com>
2024-10-28 09:49:59 +0700

    new branch

---
+++ src/tugas4/Variabel.java
@@ -0,0 +1,14 @@
+package tugas4;
+
+import java.time.LocalDate;
+
+/**
+ * Kelas Variabel menyediakan variabel dasar yang digunakan oleh kelas turunannya,
+ * seperti Person, untuk merepresentasikan data pribadi.
+ * Variabel ini termasuk nama, jenis kelamin, dan tanggal lahir.
+ */
+public class Variabel {
+    protected String name;
+    protected char gender;
+    protected LocalDate birthDate;
+}
